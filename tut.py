ce16f72af54f744242cb883f35c1754135de3728
rudraksh <bohrarudraksh2608@gmail.com>
2022-07-16 13:32:26 +0530

    initial commit

---
+++ tut.py
@@ -0,0 +1,74 @@
+# x = 5
+# y = 6
+# total = x + y
+# print(total)
+first_name = 'rudr aksh'
+last_name = 'bohra'
+# print(first_name.islower())
+# print(first_name.upper())
+# print(first_name.rsplit())
+# print(first_name.isalpha())
+# sentence = f'{first_name} {last_name} is a coder'
+# print(sentence)
+# lst = [1,2,3,4,5,'hello',['a','b','c']]
+# print(lst[0])
+# print(lst[-1])
+# print(lst[-2][0])
+# lst[0] = 100
+# print(lst)
+random_number_list = [10,32,20,45,42,41,69]
+# random_number_list.sort()
+# print(random_number_list)
+# random_number_list.sort( reverse = True)
+# print(random_number_list)
+# random_number_list.append('hello world')
+# print(random_number_list)
+# s = 'Rudraksh'
+# ls = list(s)
+# print(ls)
+# ls.sort()
+# print(ls)
+# a = [1,2,3]
+# b = [3,4,5]
+# c = a + b
+# print(c)
+# a.append(b)
+# print(a)
+# a.extend(b)
+# print(a)
+#aaj set aur dictionary krvaya(7/16/22) [print:reserved in python,print_:not reserved in py]
+# map_ = {
+#     'first_name': 'Rudraksh',
+#     'second_name': 'Bohra',
+#     'company':'regex'
+# }
+#print(map_['first_name'])
+#print(map_.get('age','not found'))
+#print(map_.get('company','not found'))
+# company = map_.pop('company')
+# print(company)
+# print(map_)
+# looping syntax
+#lst = [1,2,3,4,5,6,6,7,8,4,10]
+# for i in lst[2:]:
+#for i in 'Rudraksh':
+# for i in range(1, 10):
+#     print(i)
+# for i in range(0,len(lst),2):
+#print(lst[i])
+# for i in[1,2,3]:
+#  for j in ['a','b','c']:
+#   print(i,j)
+lst = [1,3,7,24,16,27,19]
+for i in lst[0:8:1]:
+    if(i%2!=0):
+     print(i)
+  
+
+
+
+
+
+
+
+
