0efd7f136386e76b587cc3bca74130026321a164
rudraksh <bohrarudraksh2608@gmail.com>
2022-07-17 13:21:04 +0530

    new files added

--- tut.py
+++ tut.py
@@ -2,8 +2,8 @@
 # y = 6
 # total = x + y
 # print(total)
-first_name = 'rudr aksh'
-last_name = 'bohra'
+# first_name = 'rudr aksh'
+# last_name = 'bohra'
 # print(first_name.islower())
 # print(first_name.upper())
 # print(first_name.rsplit())
@@ -16,7 +16,7 @@ last_name = 'bohra'
 # print(lst[-2][0])
 # lst[0] = 100
 # print(lst)
-random_number_list = [10,32,20,45,42,41,69]
+# random_number_list = [10,32,20,45,42,41,69]
 # random_number_list.sort()
 # print(random_number_list)
 # random_number_list.sort( reverse = True)
@@ -59,11 +59,63 @@ random_number_list = [10,32,20,45,42,41,69]
 # for i in[1,2,3]:
 #  for j in ['a','b','c']:
 #   print(i,j)
-lst = [1,3,7,24,16,27,19]
-for i in lst[0:8:1]:
-    if(i%2!=0):
-     print(i)
+# lst = [1,3,7,24,16,27,19]
+              #prime no in python assignment
+# fname = 'rudraksh'
+# lname = 'bohra'
+# if fname == 'rudraksh' and lname == 'bohra':
+#     print('yes first name is rudraksh')
+# else:
+#     print('yes last name is bohra')
+# if fname == 'rudraksh' or lname == 'bohra':
+#     print('yes first name is rudraksh')
+# else:
+#     print('no first name is not rudraksh')
+# if 'rudraksh' in ['a','b','c']:
+#     print('rudraksh is in list')
+# elif 'p' in 'rudraksh':
+#     print('p is in rudraksh')
+# elif 'k' in 'rudraksh':
+#     print('k is in rudraksh')
+# else:
+#     print('kuch nahi mila')
+# take an input from an user and print the users whose ascii value is even
+# s = input('enter a string:')
+# for i in s:
+#     if ord(i) % 2 == 0:
+#       print(i)
+  #/take 5 inputs from the users and append all those inputs in the list
+# s = int(input('enter any integer value'))
+# lst=[]
+# for i in range(s):
+#     num = input()
+#     lst.append(num)
+# print(lst)
+#/ take n*2 input from user in 2,2 sets
+no_of_elem = int(input())
+map_={
+  'int':[],
+  'str':[],
+  'float':[],
   
+}
+
+for i in range(no_of_elem):
+  dt = input('enter data type: ')
+  value=input('enter value for above data type: ')
+  if dt =='str':
+    map_['str'].append(value)
+  elif dt == 'int':
+    map_['int'].append(int(value))
+  elif dt == 'float':
+    map_['float'].append(float(value))
+  else:
+    print('please initialize a empty list for {dt}'.format(dt))
+print(map_)
+
+
+    
+ 
 
 
 
