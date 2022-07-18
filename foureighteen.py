237d4254c34778c851686a8ff77eb881e493d038
rudraksh <bohrarudraksh2608@gmail.com>
2022-07-18 17:51:56 +0530

    2 files are added

---
+++ foureighteen.py
@@ -0,0 +1,5 @@
+n = input('enter a number to check whether its palindrome or not : ')
+if n[::] == n[-1::-1]:
+    print('it is palindrome')
+else:
+    print('it is not palindrome')    
---
+++ threeeighteen.py
@@ -0,0 +1,7 @@
+#print factorial of n
+n = int(input('enter a number to find its factorial'))
+fact = 1
+for i in range(1,n+1):
+    fact = fact * i
+print(fact)    
+
